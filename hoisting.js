dfffb4e1e1fff4cdf0cdba5d9abfd1e8247845e7
srosenb <srosenb@yahoo.com>
2018-09-20 15:30:30 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,12 @@
-function callMe() {
+f
+unction callMe() {
+var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
